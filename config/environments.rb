81b1d5ac1c544ca25aaa8746e268ec4f6237bb7c
Sebastian Rodriguez <srodriguez@browning.edu>
2018-10-08 02:44:20 +0000

    heroku deploy

--- config/environments.rb
+++ config/environments.rb
@@ -1,7 +1,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-	db = URI.parse(ENV['postgres://nzoqoyhyfinbzh:9ad30a6920b4f44f8af4c4f4d910654daaed4aa8c089799b711d0b19a711fba0@ec2-54-225-97-112.compute-1.amazonaws.com:5432/d7qh62m35rrl8g
+	db = URI.parse(ENV['postgres://nzoqoyhyfinbzh:9ad30a6920b4f44f8af4c4f4d910654daaed4aa8c089799b711d0b19a711fba0@localhost:5432/d7qh62m35rrl8g
 '] || 'postgres://guestbook_user:panther@localhost:8080/guestbook')
 
 	ActiveRecord::Base.establish_connection(
